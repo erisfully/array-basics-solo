d269ce827355ff68b82f5545e7d0df27ce0a2d0e
Ashley <a.clarkem@gmail.com>
2014-06-17 14:10:58 -0600

    added completed fruits

--- fruits.rb
+++ fruits.rb
@@ -1,1 +1,18 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
+
+puts fruits.length
+
+puts fruits.index('bananas')
+
+fruits.insert(1, 'raspberries')
+
+puts fruits
+
+fruits.each{|fruits| puts fruits.length}
+
+fruits.each{|fruits| puts fruits.upcase}
+
+fruits.each{|fruits| if fruits.include?'g'
+                       puts fruits
+                       end
+                       }
